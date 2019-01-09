e7f09693afcba39ebcabed379868f26e4c3c9a12
rafal <rafal_markowski@onet.pl>
2019-01-09 17:34:54 +0100

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    entry: {
+        app: './src/scripts/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist')
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    { loader: 'file-loader', options: { name: 'images/[name].[ext]' } },
+                    'img-loader'
+                ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.s?[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { url: false, sourceMap: false } },
+                    { loader: 'sass-loader', options: { sourceMap: false } }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/index.html',
+        })
+    ],
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080
+    },
+    mode: devMode ? 'development' : 'production'
+}
